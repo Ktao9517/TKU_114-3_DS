03c39e4985351d591eb89391a83121c69d629d71
Ktao9517 <ktao9517@gmail.com>
2026-07-09 10:40:17 +0800

    Add 0709 method practice

--- 0709/OrderSystemRefactor_demo.java
+++ 0709/OrderSystemRefactor_demo.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class OrderSystemRefactor {
+public class OrderSystemRefactor_demo {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
